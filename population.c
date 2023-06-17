79b275a2a96bc212222c5695aa2bacd64053ab47
Ankur Gohain <68453512+Kill-JoyCoder@users.noreply.github.com>
2023-06-17 10:00:37 +0530

    Update population.c

--- population.c
+++ population.c
@@ -1,24 +1,30 @@
 //#include <cs50.h>
+//Code by Ankur Gohain
+//Submitted 17/06/2023
+//Thank you for opening! :D
+//PS: I didn't use the cs50.h header file here as my github codespace was quite laggy, so i typed the code in my local computer and then copied to my codespace for checking. 
 #include <stdio.h>
 
 int initial_size();
 int final_size();
-int target(int si, int sf);
+void target(int si, int sf);
 
 int main(void)
 {
     int si = initial_size();
-    do{
+    while (si<9)
+    {
         printf("> Enter again!\n");
         si = initial_size();
-    }while (si<9);
+    }
     int sf = final_size();
-    do {
+    while (sf<si)
+    {
         printf("> Population dying! Enter again!\n");
         sf = final_size();
-    }while (sf<si);
+    }
     target(si, sf);
-      
+
 }
 
 int initial_size()
@@ -26,7 +32,6 @@ int initial_size()
     int si;
     printf("- Enter initial size >_ ");
     scanf("%d", &si);
-    //printf("- Initial population \\\\ %d\n", si);
     return si;
 }
 
@@ -35,17 +40,16 @@ int final_size()
     int sf;
     printf("- Enter final size >_ ");
     scanf("%d", &sf);
-    //printf("- Final population \\\\ %d\n", sf);
     return sf;
 }
 
-int target(int si, int sf)
+void target(int si, int sf)
 {
     int temp  = 0;
-    
-    do{ 
-        si = si + si/3 - si/4;
+
+    while(si<sf){
+        si = si + si / 3 - si / 4;
         temp++;
-    }while (si<sf);
-    printf("> Number of years till the target \\\\ %d\n", temp);    
+    }
+    printf("Years: %d\n", temp);
 }
